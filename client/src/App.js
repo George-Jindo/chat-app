55d7305df6ea12b9b33d4bbc39858c0f52f34ca3
George J <g.jindo@gmail.com>
2019-09-24 14:12:02 -0700

    fixed modal on page load issue. added styling to modal content

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Main from './components/Main/Main';
 import Popup from 'reactjs-popup';
 
 class App extends Component {
-    constructor(props) {
+    constructor() {
         super();
         this.state = {
             isLoggedIn: false
@@ -16,61 +16,53 @@ class App extends Component {
         console.log('First this called');
     }
 
-    UNSAFE_componentWillReceiveProps(nextProps) {
-        if (this.props.isLoggedIn !== nextProps.isLoggedIn) {
-            console.log('Modal has popped up');
-        }
-    }
-
     render() {
-        const { isLoggedIn } = this.props;
         return (
             <div className='App'>
-                {{ isLoggedIn } && (
-                    <Popup
-                        trigger={
-                            <button className='button'> Open Modal </button>
-                        }
-                        modal
-                    >
-                        {close => (
-                            <div className='modal'>
-                                <a className='close' onClick={close}>
-                                    &times;
-                                </a>
-                                <div className='header'>
-                                    {' '}
-                                    Welcome to Chatterbox!{' '}
-                                </div>
-                                <div className='content'>
-                                    {' '}
-                                    Enter a Username to begin chatting!
-                                    <form method='POST'>
-                                        <div className='form-group'>
-                                            <input
-                                                type='text'
-                                                className='form-control'
-                                                placeholder='Username'
-                                            ></input>
-                                        </div>
-                                    </form>
-                                </div>
-                                <div className='actions'>
-                                    <button
-                                        type='submit'
-                                        className='btn-modal'
-                                        onClick={() => {
-                                            console.log('modal closed ');
-                                            close();
-                                        }}
-                                    >
-                                        Enter Chat
-                                    </button>
-                                </div>
+                <Popup
+                    defaultOpen={
+                        <button className='button'> Open Modal </button>
+                    }
+                    modal
+                >
+                    {close => (
+                        <div className='modal'>
+                            <a className='close' onClick={close}>
+                                &times;
+                            </a>
+                            <div className='header'>
+                                {' '}
+                                Welcome to Chatterbox!{' '}
                             </div>
-                        )}
-                    </Popup>
-                )}
+                            <div className='content'>
+                                {' '}
+                                <p>Enter a Username to begin chatting!</p>
+                                <br></br>
+                                <form method='POST'>
+                                    <div className='form-group'>
+                                        <input
+                                            type='text'
+                                            className='form-control'
+                                            placeholder='Username'
+                                        ></input>
+                                    </div>
+                                </form>
+                            </div>
+                            <div className='actions'>
+                                <button
+                                    type='submit'
+                                    className='btn-modal'
+                                    onClick={() => {
+                                        console.log('modal closed ');
+                                        close();
+                                    }}
+                                >
+                                    Enter Chat
+                                </button>
+                            </div>
+                        </div>
+                    )}
+                </Popup>
                 <Navbar />
                 <Main />
             </div>
