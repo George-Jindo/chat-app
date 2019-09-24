d7510a1fa6d5977866af4bfe816a1a1345c5c7fe
George J <g.jindo@gmail.com>
2019-09-24 11:31:51 -0700

    added functionality for UNSAFE willReceiveProps

--- client/src/App.js
+++ client/src/App.js
@@ -5,54 +5,72 @@ import Main from './components/Main/Main';
 import Popup from 'reactjs-popup';
 
 class App extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            isLoggedIn: false
+        };
+    }
+
     componentDidMount() {
         console.log('First this called');
     }
 
+    UNSAFE_componentWillReceiveProps(nextProps) {
+        if (this.props.isLoggedIn !== nextProps.isLoggedIn) {
+            console.log('Modal has popped up');
+        }
+    }
+
     render() {
+        const { isLoggedIn } = this.props;
         return (
             <div className='App'>
-                <Popup
-                    trigger={<button className='button'> Open Modal </button>}
-                    modal
-                >
-                    {close => (
-                        <div className='modal'>
-                            <a className='close' onClick={close}>
-                                &times;
-                            </a>
-                            <div className='header'>
-                                {' '}
-                                Welcome to Chatterbox!{' '}
+                {{ isLoggedIn } && (
+                    <Popup
+                        trigger={
+                            <button className='button'> Open Modal </button>
+                        }
+                        modal
+                    >
+                        {close => (
+                            <div className='modal'>
+                                <a className='close' onClick={close}>
+                                    &times;
+                                </a>
+                                <div className='header'>
+                                    {' '}
+                                    Welcome to Chatterbox!{' '}
+                                </div>
+                                <div className='content'>
+                                    {' '}
+                                    Enter a Username to begin chatting!
+                                    <form method='POST'>
+                                        <div className='form-group'>
+                                            <input
+                                                type='text'
+                                                className='form-control'
+                                                placeholder='Username'
+                                            ></input>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div className='actions'>
+                                    <button
+                                        type='submit'
+                                        className='btn-modal'
+                                        onClick={() => {
+                                            console.log('modal closed ');
+                                            close();
+                                        }}
+                                    >
+                                        Enter Chat
+                                    </button>
+                                </div>
                             </div>
-                            <div className='content'>
-                                {' '}
-                                Enter a Username to begin chatting!
-                                <form method='POST'>
-                                    <div className='form-group'>
-                                        <input
-                                            type='text'
-                                            className='form-control'
-                                            placeholder='Username'
-                                        ></input>
-                                    </div>
-                                </form>
-                            </div>
-                            <div className='actions'>
-                                <button
-                                    type='submit'
-                                    className='btn-modal'
-                                    onClick={() => {
-                                        console.log('modal closed ');
-                                        close();
-                                    }}
-                                >
-                                    Enter Chat
-                                </button>
-                            </div>
-                        </div>
-                    )}
-                </Popup>
+                        )}
+                    </Popup>
+                )}
                 <Navbar />
                 <Main />
             </div>
