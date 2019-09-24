5d3d8cc43ca66d7891041bac8156444eb1dfee79
George J <g.jindo@gmail.com>
2019-09-24 10:30:45 -0700

    added component did mount lifecylce and tested it in console.log

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Main.css';
 
 const main = props => (
-    <div class='container'>
+    <div className='container'>
         <div className='users grid'>Users Online</div>
         <div className='search_user grid'>Search Users</div>
         <div className='messages grid'>Messages Go Here</div>
