a7ae9c9312dc30d8e8e5258593e648a06bbbddc0
George J <g.jindo@gmail.com>
2020-03-15 13:55:50 -0700

    Refactored code and updated UI of client

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
+import './Styles.css';
 import Navbar from './components/Navbar/Navbar';
-import Main from './components/Main/Main';
-import Popup from 'reactjs-popup';
 
 class App extends Component {
     constructor() {
@@ -18,7 +16,7 @@ class App extends Component {
         console.log(e.target.value);
     };
 
-    onSearch = e => {
+    /*onSearch = e => {
         const { userName } = this.state;
 
         if (userName === '') {
@@ -28,66 +26,12 @@ class App extends Component {
         console.log('logging from onSearch');
 
         localStorage.setItem('username', userName);
-    };
-
-    componentDidMount() {
-        console.log('First this called');
-    }
+    };*/
 
     render() {
         return (
             <div className='App'>
-                <Popup
-                    defaultOpen={
-                        <button className='button'> Open Modal </button>
-                    }
-                    modal
-                >
-                    {close => (
-                        <div className='modal'>
-                            <a className='close' onClick={close}>
-                                &times;
-                            </a>
-                            <div className='header'>
-                                {' '}
-                                Welcome to Chatterbox!{' '}
-                            </div>
-                            <div className='content'>
-                                {' '}
-                                <p>Enter a Username to begin chatting!</p>
-                                <br></br>
-                                <form>
-                                    <div className='form-group'>
-                                        <input
-                                            type='text'
-                                            className='form-control'
-                                            name='username'
-                                            placeholder='Username'
-                                            onChange={this.onChange}
-                                            required
-                                        ></input>
-                                    </div>
-
-                                    <div className='actions'>
-                                        <button
-                                            type='button'
-                                            className='btn-modal'
-                                            onClick={() => {
-                                                console.log('modal closed ');
-                                                this.onSearch();
-                                                close();
-                                            }}
-                                        >
-                                            Enter Chat
-                                        </button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    )}
-                </Popup>
                 <Navbar />
-                <Main />
             </div>
         );
     }
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import './Navbar.css';
-import Logo from './chatter-logo.png';
+import { Menu, Container, Icon, Button } from 'semantic-ui-react';
 
-const navbar = props => (
-    <header>
-        <nav>
-            <div className='logo'>
-                <h4>Chatterbox App</h4>
-            </div>
-            <div className='title'>
-                <h4>Messages</h4>
-            </div>
-            <div className='logo-img'>
-                <img src={Logo} alt='site logo'></img>
-            </div>
-        </nav>
-    </header>
-);
+const NavBar = () => {
+    return (
+        <Menu fixed='top' inverted>
+            <Container>
+                <Menu.Item header>
+                    <Icon name='comments' size='large' alt='logo' />
+                    Chatterbox
+                </Menu.Item>
+                <Menu.Item name='messages' />
+                <Menu.Item name='friends' />
+            </Container>
 
-export default navbar;
+            <Menu.Menu position='right'>
+                <Menu.Item>
+                    <Button primary>Register</Button>
+                </Menu.Item>
+                <Menu.Item>
+                    <Button>Log-in</Button>
+                </Menu.Item>
+            </Menu.Menu>
+        </Menu>
+    );
+};
+
+export default NavBar;
