d42fc4735e9ba90b30c1049c78ec221676df465c
George J <g.jindo@gmail.com>
2020-10-15 10:58:32 -0700

    added styling for message texts and  added input text

--- client/src/components/Content/Main.js
+++ client/src/components/Content/Main.js
@@ -11,8 +11,38 @@ class Main extends Component {
                         <a className='rm-btn' href='#'>
                             Testing
                         </a>
+                        <a className='rm-btn' href='#'>
+                            <strong>+</strong>
+                        </a>
                     </div>
-                    <div className='chat-screen'>Hello World</div>
+                    <div className='chat-screen'>
+                        <div className='chat-body'>
+                            <div className='message'>
+                                <header>Michael:</header>
+                                <p>This is a message!</p>
+                                <footer>16:00</footer>
+                            </div>
+                            <div className='message'>
+                                <header>Scott Miller:</header>
+                                <p>
+                                    This is another message! This is another
+                                    message! This is another message! This is
+                                    another message! This is another message!
+                                    This is another message!
+                                </p>
+                                <footer>16:01</footer>
+                            </div>
+                            <div className='message'>
+                                <header>Scott Miller:</header>
+                                <p>This is another message!</p>
+                                <footer>16:01</footer>
+                            </div>
+                        </div>
+                        <div className='chat-input'>
+                            <input type='text'></input>
+                            <button>Send</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
