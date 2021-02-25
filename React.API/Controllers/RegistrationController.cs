bfbcf154b03470c0539d5429d2944d862a0e449a
George J <g.jindo@gmail.com>
2021-02-24 18:46:27 -0700

    created registration controller, connected via PM

---
+++ React.API/Controllers/RegistrationController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using React.API.ChatAppDB;
+
+namespace React.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+
+        [HttpPost]
+
+        public IActionResult PostMessage([FromBody] RegistrationInput registrationInput)
+        {
+
+            var username = registrationInput.Username;
+            var email = registrationInput.Email;
+            var password = registrationInput.Password;
+
+            var messageQueryService = new MessageQueryService();
+
+            var messages = messageQueryService.CreateMessages();
+
+            Console.WriteLine("User created succesfully");
+
+            return Ok(messages);
+        }
+    }
+
+    public class RegistrationInput
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+        public DateTime Created_At { get; set; }
+    }
+}
